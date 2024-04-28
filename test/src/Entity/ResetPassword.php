adfa2a708c4d24ba025b41cd23e322d7465f9a9d
souleimenblh <belhadjsouleimen13@gmail.com>
2024-04-28 16:48:05 +0100

    first commit

---
+++ test/src/Entity/ResetPassword.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResetPasswordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResetPasswordRepository::class)]
+class ResetPassword
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
